df6a4204a4118b0367008d8c950d5eaf4b0942da
Maurice Domínguez <maurice.ronet.dominguez@gmail.com>
2019-03-26 13:06:35 +0100

    feat: adds into and sequence transducers methods

--- src/transducers.js
+++ src/transducers.js
@@ -8,17 +8,50 @@ const filter = predicate => rf => (acc, val) => {
     return predicate(val) ? rf(acc, val) : acc;
 };
 
-const into = curry((initial, xf, xs) => {
-    return xs.reduce(xf, initial);
-});
+const take = (n, _taken = 0) => rf => (acc, val) => {
+    return ++_taken <= n ? rf(acc, val) : acc;
+};
+
+const reduce = (f, initial, xs) => {
+    let result;
+    for (let item of xs[Symbol.iterator]()) {
+        result = result ? f(result, item) : f(initial, item);
+    }
+    return result;
+};
 
 const transduce = curry((xf, rf, initial, input) => {
-    return input.reduce(xf(rf), initial);
+    return reduce(xf(rf), initial, input);
 });
+
+const sequence = (xf, xs) => {
+    return transduce(xf, xs.constructor.step, xs.constructor.initial(), xs);
+};
+
+const into = (to, xf, from) => {
+    return transduce(xf, to.constructor.step, to.constructor.initial(), from);
+};
+
+/*
+Data structures must implement iterator protocol
+and adds to constructor the methods initial and step
+
+Set.initial = () => new Set()
+Set.step = (acc, value) => acc.add(value)
+
+Array.initial = () => []
+Array.step = (acc, value) => {
+  acc.push(value);
+  return acc
+}
+
+*/
 
 module.exports = {
     map,
     filter,
-    into,
-    transduce
+    take,
+    transduce,
+    sequence,
+    into
 };
